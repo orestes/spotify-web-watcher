80e3db0a74f10db40455a50c62401cb428d6927e
Orestes Carracedo <orestes@orestes.io>
2020-05-10 22:38:29 +0200

    feat: Listen for changes in sync storage to update icon

--- src/background-handler.js
+++ src/background-handler.js
@@ -1,12 +1,18 @@
-import {getStoredValue, setIcon} from "./utils";
 import {config} from "../config";
+import {getStoredValue, setIcon} from "./utils";
 
 export class BackgroundHandler {
     async run() {
+        await this.updateIcon();
+        chrome.storage.sync.onChanged.addListener((changes, namespace) => {
+            console.log('Changes in storage', {changes, namespace});
+            this.updateIcon();
+        });
+    }
+
+    async updateIcon() {
         const userId = (await getStoredValue(config.storageKey))[config.storageKey];
         const color = userId ? 'green' : 'red';
         await setIcon(`../../icons/48x48-${color}.png`);
-
-        // TODO: Watch for changes on the stored key an change the icon accordingly
     }
 }
