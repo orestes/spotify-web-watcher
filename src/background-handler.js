a94d45b76b3e4f586845558042e16a202a73cf67
Orestes Carracedo <orestes@orestes.io>
2020-05-12 22:59:30 +0200

    Improve instructions

--- src/background-handler.js
+++ src/background-handler.js
@@ -3,24 +3,32 @@ import {getStoredValue, setIcon} from "./utils";
 
 export class BackgroundHandler {
     async run() {
-        await this.updateIcon();
+        const userId = await this.getCurrentUserId();
+        await this.updateIcon(userId);
+
         chrome.storage.sync.onChanged.addListener((changes, namespace) => {
             console.log('Changes in storage', {changes, namespace});
             this.updateIcon();
         });
-        chrome.runtime.onInstalled.addListener((details) => {
-            if (details.reason !== chrome.runtime.OnInstalledRease.INSTALL) {
-                return;
-            }
 
-            const welcomeUrl = chrome.extension.getURL("welcome.html");
-            chrome.tabs.create({url: welcomeUrl});
-        });
+        if (userId) {
+            return; // Already displayed this page
+        }
+
+        this.displayWelcomePage();
     }
 
-    async updateIcon() {
-        const userId = (await getStoredValue(config.storageKey))[config.storageKey];
+    displayWelcomePage() {
+        const welcomeUrl = chrome.extension.getURL("welcome.html");
+        chrome.tabs.create({url: welcomeUrl});
+    }
+
+    async updateIcon(userId) {
         const color = userId ? 'green' : 'red';
         await setIcon(`../../icons/48x48-${color}.png`);
+    }
+
+    async getCurrentUserId() {
+        return (await getStoredValue(config.storageKey))[config.storageKey];
     }
 }
